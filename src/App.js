eac72cb8da369e697f0a6df1cc602ac21ce9dcea
이은주 <dldmswn071918@gmail.com>
2023-08-31 11:46:29 +0900

    20230831수업

--- src/App.js
+++ src/App.js
@@ -3,28 +3,76 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main';
 import Detail from './pages/Detail';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import QuizList from './components/Quizlist';
 
 function App() {
 
-  const QuizLength = QuizList.length;
-  console.log(QuizList)
-
   const [userName, setUserName] = useState("");
+  //기본값쓸려면 ""안에쓰면 이름에 입력이 되어있다(원래 넣으면x)
   const [quizlist, setQuizList] = useState(QuizList)
-  const ChangeName = (data) =>{
-    setUserName(data)
+  const quiz = [...QuizList];
+  const [selected,setSelected] = useState(3)
+
+  const ChangeEvent = (data) =>{
+    const classValue = data.target.className;
+    const dataValue = data.target.value;
+    switch(true){
+      //includes("값") >  해당 문자열이 있는지 체크
+      case classValue.includes("name") :
+        // console.log("이름값이 바뀜") //이름입력시 이름값이 바뀜이라고 출력
+        setUserName(dataValue)
+      break;
+      case classValue.includes("random") :
+        // console.log("랜덤값이 바뀜")
+        // console.log(dataValue) //콘솔창에 0,1나옴
+        (dataValue === "1" ? setQuizList([...quizlist].sort(()=>{
+          return Math.random() - 0.5})
+          )
+           : setQuizList([...QuizList]))
+        //각각 콘솔창 랜덤,기본으로 출력된다
+        //0 ~ 1사이의 값을 반환 > 0.5의 평균값을 얻게 되어 -0.5~0.5값으로 랜덤 출력
+      break;
+      case classValue.includes("cnt") :
+        console.log("갯수값이 바뀜")
+      break;
+      case classValue.includes("type") :
+        // console.log("문제값이 바뀜")
+        // console.log(dataValue) //html클릭하면 html이라고 출력
+        (dataValue === "전체"
+        ?
+        setQuizList([...QuizList])
+        :
+        setQuizList([...QuizList].filter((e)=>{
+          return e.type === dataValue
+          }))
+        ) // 문제유형별로 선택하면 갯수도 맞게 변경
+      break;
+      default :
+      console.log("데이터가 없습니다")
+    }
+
+    // console.log(classValue)
+    // console.log(dataValue)
   }
   // console.log(quizlist)
 
+
+
+  useEffect(()=>{
+    console.log(quizlist)
+  },[quizlist]) // 배열입력시 로딩되고 딱 한번만 실행
+  //재랜더링될때마다 어떤것을 실행할때 로딩이 되고나서 실행되는 함수
+  //새로고침시 마다 계속 실행
+
+
   return (
-    <>
+
     <Routes>
-      <Route path="/" element={<Main ChangeName={ChangeName} userName={userName} QuizLength={QuizList.length} quizlist={quizlist} />}/>
+      <Route path="/" element={<Main ChangeEvent={ChangeEvent} userName={userName} quiz={quiz} quizlist={quizlist} selected={selected}/>}/>
       <Route path="/quiz" element={ <Detail quizlist={quizlist} userName={userName}/>}/>
     </Routes>
-    </>
+
     // <div className="App">
       
     // </div>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
-function Main({userName, ChangeName, QuizLength, quizlist}) {
+function Main({userName,ChangeEvent,quizlist,quiz,selected}) {
   
   let navigate = useNavigate();
 
-  // const TypeSelect = quizlist.filter()
-
-  const LengthSelectCnt = quizlist.filter(data =>{
-    return console.log(data.type);
-  })
-
+  const type = quiz.map(item => item.type);
+  // console.log(type) //html 3개,css 3개, javascript 3개 출력
+  const typeSelect = [...new Set(type)];//중복을 제거하고 새로운 배열을 만드는 코드
+  console.log(typeSelect) //html,css,javascript 딱 3개만 출력
 
   const nameChk = () =>{
 
@@ -24,10 +22,6 @@ function Main({userName, ChangeName, QuizLength, quizlist}) {
     )// 비어있지 않다면 detail로 넘어감
   }
 
-  const ChangeEvent = (e)=>{
-    // console.log(e.target)
-    ChangeName(e.target.value)
-  }
 
   return (
     <>
@@ -37,32 +31,40 @@ function Main({userName, ChangeName, QuizLength, quizlist}) {
         <div className='mx-auto basis-11/12 lg:basis-10/12'>
           <div className='bg-white rounded-lg p-5 pb-0'>
             <div className='text-center flex flex-wrap justify-between'>
-              <input defaultValue={userName} onChange={ChangeEvent} className='border pl-4 py-2 rounded-md shadow-md outline-none basis-full sm:basis-8/12 ' type="text" placeholder='이름을 입력해주세요' />
+              <input defaultValue={userName} className='name border pl-4 py-2 rounded-md shadow-md outline-none basis-full sm:basis-8/12 ' type="text" onChange={ChangeEvent} placeholder='이름을 입력해주세요' />
               <button className='text-sm sm:text-base btn-primary bg-blue-500 hover:blue-700 focus:ring-blue-400 sm:py-0 basis-full  sm:basis-3/12 mt-5 sm:mt-0' onClick={nameChk}>시작하기</button>
-              <h3 className="my-5 basis-full text-center">{userName !== "" && <><span className='text-indigo-500 font-bold'>{userName}님</span><span>문제 유형은 기본값으로 설정 되어있으며, 총 {QuizLength}문제 중 1문제를 선택하셨습니다.</span></>}</h3> 
+              <h3 className="my-5 basis-full text-center">{userName !== "" && <><span className='text-indigo-500 font-bold'>{userName}님</span><span>문제 유형은 기본값으로 설정 되어있으며, 총 {quiz.length}문제 중 1문제를 선택하셨습니다.</span></>}</h3> 
               {/* userName이 있으면 나오고, 없으면 나오지않는다 */}
             </div>
           </div>
           <div className="w-full bg-white rounded-lg p-5 mt-5 flex flex-wrap justify-between items-center">
             <div className="flex justify-around items-center xl:basis-4/12 basis-full">
               <button className='btn-primary text-sm sm:text-base bg-green-800 hover:bg-green-700 focus:ring-green-400 basis-5/12'>랜덤설정</button>
-              <select className='border rounded basis-6/12 text-center py-1.5'>
+              <select className='random border rounded basis-6/12 text-center py-1.5'  onChange={ChangeEvent}>
                 <option value="0">기본</option>
                 <option value="1">랜덤</option>
               </select>
             </div>
             <div className="flex justify-around items-center xl:basis-4/12 basis-full">
               <button className='btn-primary text-sm sm:text-base bg-green-800 hover:bg-green-700 focus:ring-green-400 basis-5/12 my-5'>갯수 설정</button>
-              <select className='border rounded basis-6/12 text-center py-1.5'>
-                <option value="0">1문제</option>
-                <option value="1">2문제</option>
+              <select className='cnt border rounded basis-6/12 text-center py-1.5'  onChange={ChangeEvent} defaultValue={selected}>
+                {
+                  Array(quizlist.length).fill().map((e,i)=>{
+                    return <option value={i+1} key={i}>{i+1}문제</option>
+                  })
+                }
               </select>
             </div>
             <div className="flex justify-around items-center xl:basis-4/12 basis-full">
               <button className='btn-primary text-sm sm:text-base bg-green-800 hover:bg-green-700 focus:ring-green-400 basis-5/12'>문제 유형</button>
-              <select className='border rounded basis-6/12 text-center py-1.5'>
-                <option value="0">전체 ({QuizLength}문제)</option>
-                <option value="1">옵션 (0문제)</option>
+              <select className='type border rounded basis-6/12 text-center py-1.5'  onChange={ChangeEvent}>
+                <option value="전체">전체 {quiz.length}(문제)</option>
+                  {
+                    typeSelect.map(el =>{
+                      return <option value={el} key={el}>{el} ({quiz.filter(e=> el===e.type).length})</option> //유형별로 각각 몇문제 있는지 확인이 된다
+                    }) 
+                  }
+                  {/* 문제유형 고를수 있다 ,html css javascript */}
               </select>
             </div>
           </div>
